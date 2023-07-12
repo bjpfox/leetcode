d5c7f8b7daa6739803933e1b30e3481bc686bd96
Brendan Fox <105548359+bjpfox@users.noreply.github.com>
2023-07-12 23:41:40 +1000

    Time: 3175 ms (5.55%), Space: 64.6 MB (15.67%) - LeetHub

--- 0015-3sum/0015-3sum.js
+++ 0015-3sum/0015-3sum.js
@@ -47,36 +47,13 @@ function binarySearch(array, target) {
   }
 }
 
-
-
-
-
-// function binarySearch(array, target) {
-//   let left = 0;
-//   let right = array.length - 1;
-
-//   while (left <= right) {
-//     let mid = Math.floor((left + right) / 2);
-
-//     if (array[mid] === target) {
-//       return mid;
-//     } else if (array[mid] < target) {
-//       left = mid + 1;
-//     } else {
-//       right = mid - 1;
-//     }
-//   }
-//   return -1;
-// }
-
 var threeSum = function(nums) {
-    nums.sort((a,b) => a - b)
-    //console.log(nums)
-    
     let triplets = {}
     let newTriplet = []
     let i = 0
     let j = 0
+
+    nums.sort((a,b) => a - b)
 
     // if the ith value is positive, there's no way we could get a triplet, since j and k must be > i, hence cant sum to 0
     while(nums[i] <= 0) {
@@ -85,24 +62,6 @@ var threeSum = function(nums) {
         // if i+j > 0, we can no longer find a triplet, since k must also be positive, hence triplet cant sum to 0
         while(nums[i] + nums[j] <= 0) {
             target = -nums[i] - nums[j]
-
-            // Check if theres a valid triplet (does the target exist in nums?)
-            // Issue: if nums[i] or nums[j] is the target, then binarySearch will find this and return it
-            // and think we've got a valid triplet
-            // it would only be valid if theres multiple vals
-            // otherwise not
-            // whats the simple way to address this issue?
-            // could slice but that would be O(n) time? as it needs to copy the array
-            // could modify binarySearch so it returns all indexes
-            // if target = nums[i] and/nums[j] check theres multiple matches 
-            // numsReduced = nums.slice(nums.length - j)
-            
-            // numsReduced = nums.slice(j + 1)
-            
-            //console.log('nums:',nums)
-            //console.log('numsReduced:',numsReduced)
-
-            //let targetIndex = binarySearch(numsReduced, target)
             let targetIndex = binarySearch(nums, target)
 
             let tripletIsValid = targetIndex !== -1
@@ -115,28 +74,21 @@ var threeSum = function(nums) {
                 // So need at least two items if one matches, e.g. the triplet -4 + 2 + 2 = 0 
                 // or three if both match, e.g. the triplet 0 + 0 = 0
                 if (target === nums[i] && target === nums[j]) {
-                    //console.log('A', targetIndex)
                     if (targetIndex.length < 3) {
-                        //console.log('B')
                         uniqueArray = false
                     }
                 } else if (target === nums[i] || target === nums[j]) {
-                        //console.log('C', targetIndex, targetIndex.length)
                     if (targetIndex.length < 2) {
-                        //console.log('D')
                         uniqueArray = false
                     }
                 }
-
                 newTriplet = [nums[i], nums[j], target].sort((a,b) => a - b)
-                //console.log('triplet valid', newTriplet)
                 
                 // Checks if triplet array already exists
                 if (triplets[newTriplet[2]]) {
                     for (let triplet of triplets[newTriplet[2]]) {
                         if (triplet[0] === newTriplet[0] && triplet[1] === newTriplet[1]) {
                             uniqueArray = false
-                            //console.log('array exists')
                             break
                         } 
                     }   
@@ -153,19 +105,8 @@ var threeSum = function(nums) {
             // Increment j to continue checking for more triplets
             j++
         }
-        // Then once all j are exhausted, increment i and start checking again from that point
+        // Once all j are exhausted, increment i and start checking again from that point
         i++
     }
     return Object.values(triplets).flat()
 };
-
-// How to reduce time complexity?
-// Why do we loop through checking all the possible sums
-// Why not set target = i + j, and then just find target k (where k = -i - j). if k exists (we dont care where), return an array [i, j, target] and then move on. to next j iteration. since nums is sorted we should be able to use binary search here. 
-
-// This code uses vars defined below, need to refactor to work this in
-// Logic is diff now, before you would check triplets to see if unique
-// Now you know you want get duplicates based on the 3rd digit (k) repeating 
-// But could still get duplicates from i, j duplicates
-    
-// Are there scenarios where target (k) could be before i,j ? no, because k must be more than i, j
