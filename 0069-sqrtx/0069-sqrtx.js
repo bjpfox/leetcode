1d7593cea49c2024f235e947ce4a42b1fb0c7c8a
Brendan Fox <105548359+bjpfox@users.noreply.github.com>
2023-07-29 10:46:33 +1000

    Time: 72 ms (67.43%), Space: 43.9 MB (35.66%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.js
+++ 0069-sqrtx/0069-sqrtx.js
@@ -6,7 +6,7 @@ var mySqrt = function(x) {
     if (x < 2) {
         return x
     }
-    for (let i = 0; i <= x; i++) {
+    for (let i = 1; i <= x; i++) {
         // Since we want sqrt rounded down to nearest integer, we know if the square of i > x, then it must be the previous i-1      which is our rounded down sqrt
         if ((i * i) > x) {
             return i - 1
