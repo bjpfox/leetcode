32f428ed88e0fcfb8e07211a5eb12a9f1e335a28
Brendan Fox <105548359+bjpfox@users.noreply.github.com>
2023-07-30 18:49:15 +1000

    Time: 45 ms (97.39%), Space: 41.9 MB (42.83%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -0,0 +1,37 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+
+// Strategy:
+// We know that either the final step involves taking either a double step or a single step
+// To finish with a double step, we must have been two steps back, hence the number of ways is just however many ways there were to get to the (i-2)th step 
+// Similarly, to finish with a single step, the number of ways is however many ways there were to get to the (i-1)th step. Hence we can say that:
+// # of ways to get to ith step = # of ways to get to i-1th step + # of ways to get to i-2th step
+
+// Plan: 
+// use stepCounter object to track (i-1th) and (i-2)th step counts
+// Loop through to our n-th step, adding counts for i-1 and i-2, as per the formula above
+// Return the overall count when we reach i=n
+
+var climbStairs = function(n) {
+    // For n = 2, we have 2 ways (1-1, 2). For n = 1, we have 1 way (1)
+    if (n <= 2) {
+        return n
+    } else {
+        // Initialise for n = 1, n = 2, per above
+        let stepCounter = {
+            currentCount: 2, 
+            previousCount: 1 
+        }
+        let currentStepCount = stepCounter.currentCount + stepCounter.previousCount
+
+        for (let i = 3; i <= n; i++) {
+            currentStepCount = stepCounter.currentCount + stepCounter.previousCount
+            stepCounter.previousCount = stepCounter.currentCount
+            stepCounter.currentCount = currentStepCount
+        }
+        return currentStepCount
+    }
+};
+
